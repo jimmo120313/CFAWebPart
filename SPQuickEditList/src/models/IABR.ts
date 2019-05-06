fadc916049351c23f763621029193cfff44c0432
Jimmy Mo <jimmo120313@hotmail.com>
2019-05-06 17:32:13 +1000

    Add

--- SPQuickEditList/src/models/IABR.ts
+++ SPQuickEditList/src/models/IABR.ts
@@ -1,15 +1,15 @@
-export interface IListABR {
+export interface IABR {
   id: string;
-  brigadeId: string;
-  brigadeName: string;
-  reviewPeriod: string;
-  dateStarted: Date;
-  completedBy: string;
-  districtId: string;
-  districtName: string;
-  regionId: string;
-  regionName: string;
-  actionPlanReportURL: URL;
-  reviewDetail: URL;
   reviewId: string;
+  brigadeId?: string;
+  brigadeName?: string;
+  reviewPeriod: string;
+  dateStarted?: string;
+  completedBy?: string;
+  districtId?: string;
+  districtName?: string;
+  regionId?: string;
+  regionName?: string;
+  actionPlanReportURL?: string;
+  reviewDetail?: string;
 }
